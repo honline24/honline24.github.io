b9aef863b427c19fa012d89561e888730e47b518
sofiatripichio <sofiatripichio@gmail.com>
2024-08-06 00:35:29 -0300

    yo

--- contactoscript.js
+++ contactoscript.js
@@ -1,18 +1,41 @@
-const form = document.getElementById('contactForm');
-const submitButton = document.getElementById('submitButton');
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contactForm');
+    const submitButton = document.getElementById('submitButton');
 
-form.addEventListener('input', function() {
-    let allFieldsFilled = true;
-    Array.from(form.elements).forEach(function(element) {
-        if (element.type !== "submit" && element.value === "") {
-            allFieldsFilled = false;
-        }
+    form.addEventListener('input', function() {
+        let allFieldsFilled = true;
+        Array.from(form.elements).forEach(function(element) {
+            if (element.type !== "submit" && element.value === "") {
+                allFieldsFilled = false;
+            }
+        });
+        submitButton.disabled = !allFieldsFilled;
     });
-    submitButton.disabled = !allFieldsFilled;
-});
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulario enviado con éxito!');
-    // Aquí puedes agregar el código para enviar el formulario a tu servidor.
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        fetch(form.action, {
+            method: form.method,
+            body: new FormData(form),
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                alert('Formulario enviado con éxito!');
+                form.reset();
+                submitButton.disabled = true;
+            } else {
+                response.json().then(data => {
+                    if (Object.hasOwn(data, 'errors')) {
+                        alert(data["errors"].map(error => error["message"]).join(", "));
+                    } else {
+                        alert('Ocurrió un error al enviar el formulario.');
+                    }
+                });
+            }
+        }).catch(error => {
+            alert('Ocurrió un error al enviar el formulario.');
+        });
+    });
 });
