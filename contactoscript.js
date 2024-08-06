11a7d615b8bf2ded9eea76b1e9f9d53a47722327
sofiatripichio <sofiatripichio@gmail.com>
2024-08-06 00:51:51 -0300

    yo

--- contactoscript.js
+++ contactoscript.js
@@ -1,41 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('contactForm');
-    const submitButton = document.getElementById('submitButton');
+ddocument.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("contact-form");
 
-    form.addEventListener('input', function() {
-        let allFieldsFilled = true;
-        Array.from(form.elements).forEach(function(element) {
-            if (element.type !== "submit" && element.value === "") {
-                allFieldsFilled = false;
-            }
-        });
-        submitButton.disabled = !allFieldsFilled;
-    });
+    form.addEventListener("submit", function(event) {
+        // Obtener todos los elementos que son requeridos
+        const requiredFields = [
+            form.nombre,
+            form.apellido,
+            form.telefono,
+            form.email,
+            form.mensaje
+        ];
 
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        fetch(form.action, {
-            method: form.method,
-            body: new FormData(form),
-            headers: {
-                'Accept': 'application/json'
+        // Verificar si se llenaron todos los campos requeridos
+        const isValid = requiredFields.every(field => {
+            if (field.type === 'radio') {
+                return Array.from(document.getElementsByName(field.name)).some(radio => radio.checked);
             }
-        }).then(response => {
-            if (response.ok) {
-                alert('Formulario enviado con éxito!');
-                form.reset();
-                submitButton.disabled = true;
-            } else {
-                response.json().then(data => {
-                    if (Object.hasOwn(data, 'errors')) {
-                        alert(data["errors"].map(error => error["message"]).join(", "));
-                    } else {
-                        alert('Ocurrió un error al enviar el formulario.');
-                    }
-                });
-            }
-        }).catch(error => {
-            alert('Ocurrió un error al enviar el formulario.');
+            return field.value.trim() !== '';
         });
+
+        if (!isValid) {
+            event.preventDefault(); // Detener el envío del formulario
+            alert("Por favor, complete todos los campos obligatorios antes de enviar el formulario.");
+        }
     });
 });
